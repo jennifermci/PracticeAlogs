f036042d76a87927d61ef3eedade1fbceaf6b75b
Jennifer McIlvaine <jennymci22@gmail.com>
2020-05-21 12:39:42 -0700

    fixed BST

--- BSTmerge.js
+++ BSTmerge.js
@@ -24,3 +24,23 @@ var mergeTrees = function(t1, t2, newNode = new TreeNode()) {
     return newNode;
     
 };
+
+//This is the correct way
+var mergeTrees = function(t1, t2) {
+
+    if(!t1){
+        return t2;
+    }
+    
+    if(!t2){
+        return t1;
+    }
+    
+    t1.val += t2.val;
+    
+    t1.left = mergeTrees(t1.left, t2.left)
+    t1.right = mergeTrees(t1.right, t2.right)
+
+    return t1;
+    
+};
