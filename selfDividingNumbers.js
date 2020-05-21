a99d11b85bd7090d6804e076b4e03e5045d89537
Jennifer McIlvaine <jennymci22@gmail.com>
2020-05-21 12:32:48 -0700

    practiced some BST

--- selfDividingNumbers.js
+++ selfDividingNumbers.js
@@ -2,9 +2,11 @@ var selfDividingNumbers = function(left, right) {
     var numberArr = [];
     
     for(var i=left; i<=right; i++){
+        //turns the numbers into strings
         var num = i.toString();
         var flag = false;
         for(var x=0; x<num.length; x++){
+            //turns the string into numbers
             var Num = parseInt(num[x]);
             if(Num == 0){
                 flag = true;
